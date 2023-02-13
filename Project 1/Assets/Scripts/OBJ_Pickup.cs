8f3159ebe9179850efda45913e7b0178c48096cd
SquidJit <evanschmitz11@gmail.com>
2023-02-12 19:09:47 -0500

    Added more collectible code

--- Project 1/Assets/Scripts/OBJ_Pickup.cs
+++ Project 1/Assets/Scripts/OBJ_Pickup.cs
@@ -1,24 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using TMPro;
 
 
 public class OBJ_Pickup : MonoBehaviour
 {
     public GameObject pickup_Canvas;
-   
+    public bool canvasActive;
+    public GameObject startCollectible;
+    public GameObject personCollectible;
+    public bool prismActive;
+    public bool pipeActive;
+    public bool torusActive;
+
     // Start is called before the first frame update
     void Start()
     {
         pickup_Canvas.SetActive(false);
+        startCollectible.SetActive(true);
+        personCollectible.SetActive(false);
+        prismActive = false;
+        pipeActive = false;
+        torusActive = false;
+
     }
 
-    private void OnColliderEnter(Collider other)
+    void Update()
     {
-        if (other.tag == "Player")
+        if (canvasActive == true)
         {
-            pickup_Canvas.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                startCollectible.SetActive(false);
+                personCollectible.SetActive(true);
+                pickup_Canvas.SetActive(false);
+
+                if (startCollectible.tag == "Prism")
+                {
+                    prismActive = true;
+
+                }
+                else
+                {
+                    if (startCollectible.tag == "Pipe")
+                    {
+
+                        pipeActive = true;
+                    }
+
+                    else
+                    {
+                        if (startCollectible.tag == "Torus")
+                        {
+
+                            torusActive = true;
+                        }
+                    }
+                }
+            }
         }
     }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                pickup_Canvas.SetActive(true);
+                canvasActive = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                pickup_Canvas.SetActive(false);
+                canvasActive = false;
+            }
+        }
+    
 }
